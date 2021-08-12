3ab0c23ddbf13ba5c82126befbad8450c82966ff
Shobhit Kasliwal <SKasliwal@liventus.com>
2021-08-11 19:43:00 -0500

    u

---
+++ .github/workflows/charts/fakeapi/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+args="$@"
+
+args="$@ -p 4301"
+
+file=/fakeapi-data/db.json
+if [ -f $file ]; then
+    echo "Found db.json, trying to open"
+    args="$args db.json"
+fi
+
+file=/fakeapi-data/file.js
+if [ -f $file ]; then
+    echo "Found file.js seed file, trying to open"
+    args="$args file.js"
+fi
+
+file=/fakeapi-data/route.json
+if [ -f $file ]; then
+    echo "Found route.json route file, trying to open"
+    args="$args -r route.json"
+fi
+
+json-server $args
